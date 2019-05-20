daf23d48b1087d820e41debb775aaac49512c544
Duncan McGreggor <oubiwann@gmail.com>
2019-05-20 17:55:34 -0500

    CI/CD support.

--- project.clj
+++ project.clj
@@ -1,8 +1,19 @@
-(defproject defun "0.3.0-RC1"
-  :description "A macro to define clojure functions with parameter pattern matching just like erlang or elixir."
-  :url "https://github.com/killme2008/defun"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/core.match "0.3.0-alpha4"]
-                 [org.clojure/tools.macro "0.1.2"]])
+(defproject clojusc/defun "0.3.0"
+  :description "A Clojure macro supporting functions with pattern matching heads a la LFE"
+  :url "https://github.com/clojusc/defun"
+  :license {
+    :name "Eclipse Public License"
+    :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [
+    [org.clojure/clojure "1.8.0"]
+    [org.clojure/core.match "0.3.0-alpha4"]
+    [org.clojure/tools.macro "0.1.2"]]
+  :profiles {
+    :ubercompile {
+      :aot :all}
+    :test {
+      :plugins [
+        [lein-ltest "0.3.0"]]}}
+  :aliases {
+    "ubercompile" ["with-profile" "+ubercompile" "compile"]
+    "test" ["with-profile" "+test" "ltest"]})
